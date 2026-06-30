53cdb7c17c2db280e66d1ff775a157d258587448
forsidenis <forsidenis@yandex.ru>
2026-06-30 22:28:30 +0300

    Этап 1 (выполнен)

--- stat/stats-server/src/main/java/ru/practicum/stat/server/StatServer.java
+++ stat/stats-server/src/main/java/ru/practicum/stat/server/StatServer.java
@@ -2,7 +2,9 @@ package ru.practicum.stat.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StatServer {
     public static void main(String[] args) {
