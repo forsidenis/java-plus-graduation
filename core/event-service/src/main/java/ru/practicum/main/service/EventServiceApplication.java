b7272146c5c55708f66b29a4381d660274e7c91a
forsidenis <forsidenis@yandex.ru>
2026-07-07 21:10:27 +0300

    Этап 2 (выполнен)

--- core/event-service/src/main/java/ru/practicum/main/service/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/main/service/EventServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import ru.practicum.stat.client.StatsClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@Import(StatsClient.class)
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
