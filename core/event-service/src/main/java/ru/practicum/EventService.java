c1c74e8b650cf9631b124647081f5b951233804d
forsidenis <forsidenis@yandex.ru>
2026-07-12 23:18:07 +0300

    Этап 2 (выполнен)

--- core/event-service/src/main/java/ru/practicum/EventService.java
+++ core/event-service/src/main/java/ru/practicum/EventService.java
@@ -2,11 +2,15 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import ru.practicum.exception.ErrorHandler;
 import ru.practicum.stat.client.StatsClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @Import({StatsClient.class, ErrorHandler.class})
 public class EventService {
     public static void main(String[] args) {
