f9a1de99918badd99dcba6b248f4e924647ebaad
forsidenis <forsidenis@yandex.ru>
2026-08-01 00:15:39 +0300

    Рекомендательный сервис (выполнен)

--- recommendations/aggregator/src/main/java/ru/practicum/Aggregator.java
+++ recommendations/aggregator/src/main/java/ru/practicum/Aggregator.java
@@ -3,17 +3,20 @@ package ru.practicum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.practicum.controller.AggregationStarter;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Aggregator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         log.info("Запуск Aggregator");
         ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+        Thread.sleep(5000);
         AggregationStarter aggregator = context.getBean(AggregationStarter.class);
         new Thread(aggregator::start).start();
-        log.info("Aggregator запущен, регистрация в Eureka продолжается");
+        log.info("Aggregator запущен, регистрация в Eureka выполнена");
     }
 }
