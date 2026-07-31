d9f2a73c6d5dc7f4e4e352698b64a0a04d203f10
forsidenis <forsidenis@yandex.ru>
2026-07-31 20:44:06 +0300

    Рекомендательный сервис (выполнен)

--- recommendations/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
+++ recommendations/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
@@ -8,13 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class AnalyzerApplication {
     public static void main(String[] args) {
-        System.setProperty("spring.datasource.url", "jdbc:h2:mem:analyzer;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-        System.setProperty("spring.datasource.driver-class-name", "org.h2.Driver");
-        System.setProperty("spring.datasource.username", "sa");
-        System.setProperty("spring.datasource.password", "");
-        System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
-        System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
-
         SpringApplication.run(AnalyzerApplication.class, args);
     }
 }
